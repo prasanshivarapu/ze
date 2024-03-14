be5ffa6b5dd567f030c7a84f0a9930cd9e9c462d
PrashanthKJP <prashanthprash969@gmail.com>
2024-03-14 12:22:42 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -4,23 +4,22 @@ import NavbarComp from "./components/Navbar/index"; // Assuming NavbarComp is in
 import Homepage from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
 import AccountOpenSection from "./components/Account/Account";
-import NavbarComp1 from "./components/New";
 
 function App() {
-    return (
-        <Router>
-            <div>
-                <NavbarComp />
-                <Routes>
-                    <Route path="/" element={<Homepage />} />
+  return (
+    <Router>
+      <div>
+        <NavbarComp />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
 
-                    <Route path="/signup" element={<AccountOpenSection />} />
-                    {/* <Route path="/1" element={<NavbarComp1 />} /> */}
-                </Routes>
-                <Footer />
-            </div>
-        </Router>
-    );
+          <Route path="/signup" element={<AccountOpenSection />} />
+          {/* <Route path="/1" element={<NavbarComp1 />} /> */}
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
