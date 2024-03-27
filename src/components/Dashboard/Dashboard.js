7bbdfba0aabbaa49902f46ab5904d1b45ef1e81c
PrashanthKJP <prashanthprash969@gmail.com>
2024-03-27 16:00:19 +0530

    dashboard work

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { Container, Row, Col, Button, Navbar, Nav } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Navbar,
+  Nav,
+  Card,
+} from "react-bootstrap";
 import { TbArrowBigLeftFilled } from "react-icons/tb";
 import { IoIosNotificationsOutline } from "react-icons/io";
 import { CgProfile } from "react-icons/cg";
@@ -9,16 +17,49 @@ const Dashboard = () => {
   return (
     <Container fluid>
       <Row>
-        <Col md={3}>sidebar</Col>
-        <Col md={9}>
-          <Navbar
-            sticky="top"
-            collapseOnSelect
-            bg="light"
-            expand="lg"
-            className="bg-body-tertiary "
-          >
-            <Container fluid style={{ margin: "0.5vw 5vw" }}>
+        <Navbar
+          sticky="top"
+          collapseOnSelect
+          bg="light"
+          expand="lg"
+          className="bg-body-tertiary "
+          style={{ border: "1px solid black" }}
+        >
+          <Col md={3} style={{ borderRight: "1px solid black" }}>
+            <Container
+              fluid
+              style={{
+                display: "flex",
+                justifyContent: "space-around",
+              }}
+            >
+              <Nav className="nam">
+                <Nav.Link
+                  className="nav-links "
+                  href="/dashboard"
+                  style={{ fontSize: "1vw" }}
+                >
+                  NIFTY 50 <span style={{ color: "orange" }}>21983.08</span>
+                </Nav.Link>
+                <Nav.Link
+                  className="nav-links "
+                  href="/dashboard"
+                  style={{ fontSize: "1vw" }}
+                >
+                  NIFTY 50 <span style={{ color: "orange" }}>21983.08</span>
+                </Nav.Link>
+              </Nav>
+            </Container>
+          </Col>
+          <Col md={9}>
+            <Container
+              fluid
+              style={{
+                margin: "0.5vw 5vw",
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
               <Navbar.Brand className="nav-links " href="/">
                 <TbArrowBigLeftFilled />
               </Navbar.Brand>
@@ -53,7 +94,59 @@ const Dashboard = () => {
                 </Nav.Link>
               </Nav>
             </Container>
-          </Navbar>
+          </Col>
+        </Navbar>
+      </Row>
+      <Row>
+        <Col md={3}>hi</Col>
+        <Col
+          md={9}
+          style={{
+            borderLeft: "1px solid black",
+            height: "100vh",
+          }}
+        >
+          <h1>Hi, Arvinder</h1>
+          <hr />
+          <Row>
+            <Col md={3}>
+              <h1>0</h1>
+              <h5>Margin availbel</h5>
+            </Col>
+            <Col
+              md={6}
+              style={{
+                display: "flex",
+                justifyContent: "space-around",
+                alignItems: "center",
+              }}
+            >
+              <div>
+                <h6>
+                  Margin used <span>0</span>
+                </h6>
+                <h6>
+                  Opening balence <span>0</span>
+                </h6>
+                <h6>View Statement</h6>
+              </div>
+              <div>
+                <h1>0</h1>
+                <h5>Margin availbel</h5>
+              </div>
+            </Col>
+            <Col md={3}>
+              <div>
+                <h6>
+                  Margin used <span>0</span>
+                </h6>
+                <h6>
+                  Opening balence <span>0</span>
+                </h6>
+                <h6>View Statement</h6>
+              </div>
+            </Col>
+          </Row>
         </Col>
       </Row>
     </Container>
